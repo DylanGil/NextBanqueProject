ab19012b93324a68181cff5d3bf4516418f7aa76
Dylan GIL AMARO <dylan.flame.libra@gmail.com>
2022-01-18 13:20:54 +0100

    TrollCommit

--- pages/test.js
+++ pages/test.js
@@ -4,5 +4,5 @@ import styles from "../styles/Home.module.css";
 import Main from "./src/components/Main";
 
 export default function Home() {
-  return <Main name="Dylan" />;
+  return <Main name="Sam le belge" />;
 }
