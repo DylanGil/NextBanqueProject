359e109e27158cb229ed95e885f37631abfc6ae7
DylanGil <dylan.flame.libra@gmail.com>
2022-01-19 10:49:08 +0100

    forgot good path

--- pages/add-entry.js
+++ pages/add-entry.js
@@ -1,6 +1,6 @@
-import Test from "./src/components/Main";
-import Header from "./src/components/Header";
-import Main from "./src/components/Main";
+import Test from "../src/components/Main";
+import Header from "../src/components/Header";
+import Main from "../src/components/Main";
 
 export default function Home() {
   return (
